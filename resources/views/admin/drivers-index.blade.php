5bf1a726e9688b1b261cd7cf23090528e514f8a6
Alexander loperena <alexandersauna05@gmail.com>
2024-10-18 11:48:17 -0500

    Se agrego cambios en el archivo de reportes

--- resources/views/admin/drivers-index.blade.php
+++ resources/views/admin/drivers-index.blade.php
@@ -11,20 +11,26 @@
             {{ session('message') }}
         </div>
         @endif
-
-
-
         {{-- Listado de Conductores --}}
         @if ($drivers && $drivers->isNotEmpty())
             @foreach ($drivers as $driver)
                 <a class="flex w-2/4 border-b-2 border-gray-500 hover:bg-gray-300/50 rounded-md p-2 space-x-5" href="{{route('admin.driver_show',$driver->id)}}">
                     <div class="flex justify-center items-center">
-                        @if($driver->imagen) 
-                            <img src="{{ asset('storage/' . $driver->imagen) }}" class="w-36" alt="{{ $driver->user->name }}">
+                        {{-- @if($driver->imagen) 
+                            <img src="{{ asset('storage/' . URL('')) }}" class="w-36" alt="{{ $driver->user->name }}">
+                            <img src="{{ asset('storage/images/0sHSp7lEYukbc2SDDuIV6aVDqy1i7C6RBnpf8l0j.jpg') }}" alt="">
                         @else
                             <img src="{{ asset('images/conductor-por-defecto.png') }}" class="w-36" alt="Conductor por defecto">
-                        @endif
+                        @endif --}}
+                        <img id="driver-image" class="w-36" alt="Conductor">
 
+                            <script>
+                                const driverImage = '{{ $driver->imagen }}';
+                                const defaultImage = '{{ asset('images/conductor-por-defecto.png') }}';
+
+                                document.getElementById('driver-image').src = driverImage ? '{{ asset('storage/') }}' + driverImage : defaultImage;
+                            </script>
+                                                    
                     </div>
                     <article class="space-y-5 self-center">
                         <div class="flex space-x-5">
@@ -48,6 +54,7 @@
                         </div>
                     </article>
                 </a>
+           +
             @endforeach
         @else
         <div class="text-center text-lg font-semibold text-gray-700 bg-yellow-200 border border-yellow-400 rounded p-4">
